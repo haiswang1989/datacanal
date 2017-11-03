ec6da2cba497c8051075f0c3e3cffab1abdc9645
hansen.wang <hansen.wang@vipshop.com>
2017-11-03 14:47:28 +0800

    first commit

--- datacanal-center/src/main/java/com/canal/center/netty/handler/HeartbeatHandler.java
+++ datacanal-center/src/main/java/com/canal/center/netty/handler/HeartbeatHandler.java
@@ -1,12 +1,10 @@
 package com.canal.center.netty.handler;
 
 import com.canal.center.cache.ChannelHeartbeatCache;
-import com.canal.serializer.intf.ISerializer;
 import com.datacanal.common.model.Command;
 import com.datacanal.common.model.EventType;
 import com.datacanal.common.model.Heartbeat;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
@@ -19,28 +17,23 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class HeartbeatHandler extends ChannelHandlerAdapter {
     
-    private ISerializer<Heartbeat> serializer;
-    
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        
         Command command = (Command)msg;
         EventType eventType = command.getEventType();
         
-        /*
-        ByteBuf byteBuf = (ByteBuf)msg;
-        byte[] requestByte = new byte[byteBuf.readableBytes()];
-        byteBuf.readBytes(requestByte);
-        Heartbeat heartbeat = this.serializer.decode(requestByte, Heartbeat.class);
-        
-        String nodeId = heartbeat.getNodeId();
-        Channel channel = ctx.channel();
-        long lastHeartbeatTime = heartbeat.getHeartbeatTime();
-        
-        //更新缓存中的channel以及心跳信息
-        ChannelHeartbeatCache cache = ChannelHeartbeatCache.instance();
-        cache.getNodeIdToChannel().put(nodeId, channel);
-        cache.getNodeIdToBeatTime().put(nodeId, lastHeartbeatTime);
-        */
+        if(eventType == EventType.HEART_BEAT) {
+            Heartbeat heartbeat = (Heartbeat)command.getObj();
+            String nodeId = heartbeat.getNodeId();
+            Channel channel = ctx.channel();
+            long lastHeartbeatTime = heartbeat.getHeartbeatTime();
+            
+            //更新缓存中的channel以及心跳信息
+            ChannelHeartbeatCache cache = ChannelHeartbeatCache.instance();
+            cache.getNodeIdToChannel().put(nodeId, channel);
+            cache.getNodeIdToBeatTime().put(nodeId, lastHeartbeatTime);
+        } else {
+            ctx.fireChannelRead(msg);
+        }
     } 
 }
