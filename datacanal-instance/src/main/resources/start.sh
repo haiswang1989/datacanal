8491a8913b4e46244224892e6d84cd8761fa0b5a
hansen.wang <hansen.wang@vipshop.com>
2017-11-13 10:59:12 +0800

    start shell

--- datacanal-instance/src/main/resources/start.sh
+++ datacanal-instance/src/main/resources/start.sh
@@ -8,6 +8,8 @@ dbname=${5}
 sensitiveTables=${6}
 zkPath=${7}
 
+identity=${host}_${dbname}
+
 for name in /home/instance/lib/*.jar  
 do  
    CLASSPATH="$CLASSPATH":"$name"  
@@ -16,5 +18,5 @@ done
 JAVA_OPTS="-Xms1024m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:CMSFullGCsBeforeCompaction=1 -XX:CMSInitiatingOccupancyFraction=70 -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:logs/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=logs/mtdperf.hprof -server -Dfile.encoding=UTF-8"
 main_class=com.canal.instance.CDCEngine
 
-echo "nohup java ${JAVA_OPTS} -server -classpath ${CLASSPATH} ${START_CLASS}  -h ${host} -p ${port} -u ${username} -pw ${password} -n ${dbname} -st ${sensitiveTables} -zp ${zkPath} &"
-nohup java ${JAVA_OPTS} -server -classpath ${CLASSPATH} ${main_class}  -h ${host} -p ${port} -u ${username} -pw ${password} -n ${dbname} -st ${sensitiveTables} -zp ${zkPath} &
+nohup java ${JAVA_OPTS} -server -classpath ${CLASSPATH} ${main_class}  -h ${host} -p ${port} -u ${username} -pw ${password} -n ${dbname} -st ${sensitiveTables} -zp ${zkPath} 1>>${identity}.log 2>&1 &
+echo $! > ${identity}.pid
