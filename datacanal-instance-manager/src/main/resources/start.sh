e02acd64306a8a3a9a93d795067ed43b83829f51
hansen.wang <hansen.wang@vipshop.com>
2017-11-15 11:07:36 +0800

    add code

--- datacanal-instance-manager/src/main/resources/start.sh
+++ datacanal-instance-manager/src/main/resources/start.sh
@@ -12,13 +12,9 @@ zkPath=${7}
 
 identity=${host}_${dbname}
 
-for name in /home/datacanal/datacanal-instance/lib/*.jar  
-do  
-   CLASSPATH="$CLASSPATH":"$name"  
-done  
-
 JAVA_OPTS="-Xms1024m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:CMSFullGCsBeforeCompaction=1 -XX:CMSInitiatingOccupancyFraction=70 -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:logs/gc.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=logs/mtdperf.hprof -server -Dfile.encoding=UTF-8"
-main_class=com.canal.instance.CDCEngine
 
-nohup java ${JAVA_OPTS} -server -classpath ${CLASSPATH} ${main_class}  -h ${host} -p ${port} -u ${username} -pw ${password} -n ${dbname} -st ${sensitiveTables} -zp ${zkPath} 1>>${curr_dir}/${INSTANCE_LOG_PATH}.log 2>&1 &
+main_class=com.canal.instance.code.CDCEngine
+
+nohup java ${JAVA_OPTS} -server -classpath ${CLASSPATH} ${main_class}  -h ${host} -p ${port} -u ${username} -pw ${password} -n ${dbname} -st ${sensitiveTables} -zp ${zkPath} 1>>${INSTANCE_LOG_PATH}/${identity}.log 2>&1 &
 echo $! > ${INSTANCE_LOG_PATH}/${identity}.pid
