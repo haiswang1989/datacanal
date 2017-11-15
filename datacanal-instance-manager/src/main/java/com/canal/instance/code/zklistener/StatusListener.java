13b0c068955e153c699305feef2d7742a46b264b
hansen.wang <hansen.wang@vipshop.com>
2017-11-15 14:04:50 +0800

    add log

--- datacanal-instance-manager/src/main/java/com/canal/instance/code/zklistener/StatusListener.java
+++ datacanal-instance-manager/src/main/java/com/canal/instance/code/zklistener/StatusListener.java
@@ -3,6 +3,8 @@ package com.canal.instance.code.zklistener;
 import java.util.concurrent.TimeUnit;
 
 import org.I0Itec.zkclient.IZkDataListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.canal.instance.code.handler.keeper.PositionKeeper;
 import com.datacanal.common.model.Status;
@@ -15,6 +17,8 @@ import com.datacanal.common.model.Status;
  */
 public class StatusListener implements IZkDataListener {
     
+    public static final Logger LOG = LoggerFactory.getLogger(StatusListener.class);
+    
     private int stopAfterGetCmd;
     
     public StatusListener(int stopAfterGetCmdArg) {
@@ -25,9 +29,11 @@ public class StatusListener implements IZkDataListener {
     public void handleDataChange(String dataPath, Object data) throws Exception {
         Status newStatus = (Status)data;
         if(newStatus == Status.STOP) {
+            LOG.info("Get stop myself command ,prepare to stop.");
             PositionKeeper.setStatus(newStatus);
             //等待position同步到zk
             TimeUnit.SECONDS.sleep(stopAfterGetCmd * 2);
+            LOG.info("Stop myself.");
             //终止虚拟机
             System.exit(0);
         }
